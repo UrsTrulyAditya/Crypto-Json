3382016778e3010569de8ed4eccbd68cd9b8bf20
UrsTrulyAditya <adityapavangollapudi@gmail.com>
2022-08-25 12:48:39 +0530

    cdpage updated

--- src/components/crptoDetailsPage.jsx
+++ src/components/crptoDetailsPage.jsx
@@ -1,38 +1,44 @@
-import { color } from '@mui/system';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
-import { useParams } from 'react-router-dom'
+import {  useParams } from 'react-router-dom'
 
-const CryptoDetailPage = () => {
+const CryptoDetailPage = ({imgData}) => {
     const { id } = useParams();
     const [coins, setcoin] = useState({});
+    const [img,setImage]=useState("");
 
-
-    // console.log(coins.description,coins.image.large)
+     
     useEffect(() => {
         const getCoins = () => {
             axios.get(`https://api.coingecko.com/api/v3/coins/${id}?tickers=false&market_data=true`).then((res) => {
-                setcoin(res.data)
-    
+                setcoin(res.data);
+                setImage(res.data.image.large);
+                
             }).catch((err) => console.log(err));
         }
-
+        
         getCoins();
     }, [])
-
-    // const image=coins.image.large;
-    //  const desc=coins.description.en.split(". ")[0];
-    //  console.log(image)
+    
+    
     return (
         < div className='container mt-5 mb-5'>
             <div className='row'>
                 <div className='col-4 text-white text-left'>
-                    <img src="{image}"  alt="logo" />
+                    <div>
+                    <img src={img} className="imgDetail" alt="logo" />
+ 
+                    </div>
+                     
+                    <div>
                     <p>Coin Info</p>
                     <h1>{coins.name}</h1>
-                    <div>
-                        <p>desc</p>
+                        <p> {coins.name} is the first successful internet money based on peer-to-peer technology, 
+                            whereby no central bank or authority is involved in the transaction and 
+                            production of the {coins.name} currency. 
+                            The source code is available publicly as an open source project, 
+                            anybody can look at it and be part of the developmental process.</p>
                         <p>Rank : <span className='text-info'>{coins.market_cap_rank}</span></p>
                         <span className='text-info'>{coins.price_change_24h}</span>
                         <span>{coins.current_price}</span>
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -4,32 +4,32 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Link,useNavigate } from 'react-router-dom';   
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import './table.css';
-import axios from "axios"; 
+import axios from "axios";
 
 
 const DynamicTable = () => {
     const [data, setData] = React.useState([]);
     const [saveddata, setSavedData] = React.useState([]);
-      const [search, setSearch] = React.useState('');
-      const navigate=useNavigate();
- 
+    const [search, setSearch] = React.useState('');
+    const navigate = useNavigate();
+
     React.useEffect(() => {
 
         axios
             .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
             .then((res) => {
                 setData(res.data);
-                setSavedData(res.data.slice(0,10))
+                setSavedData(res.data.slice(0, 10))
 
             })
             .catch((error) => {
                 console.log(error);
             });
 
-       
+
 
     }, []);
 
@@ -39,81 +39,83 @@ const DynamicTable = () => {
 
     const filteredData = data.filter(each => each.name.toLowerCase().includes(search.toLowerCase()))
 
-    const selected = (name, symbol, market_cap, current_price,image) => {
+    const selected = (name, symbol, market_cap, current_price, image) => {
         // console.log("clicked")
-        axios.post("https://json-server5.herokuapp.com/savedData", { name, symbol, market_cap, current_price,image }).then(() => {
+        axios.post("https://json-server5.herokuapp.com/savedData", { name, symbol, market_cap, current_price, image }).then(() => {
             // console.log("done")
         }).catch((err) => console.log(err));
 
         alert("crypto has been saved");
-        
- 
-    } 
+
+
+    }
+
     return (
         <>
-        
-        <div className="TableContainer container text-center" component={Paper}>
-            <div className="searchBox">
-                <p className='text-light'>Stock Details Table</p>
 
-                <div>
+            <div className="TableContainer container text-center" component={Paper}>
+                <div className="searchBox">
+                    <p className='text-light'>Stock Details Table</p>
 
-                    <input type="text" value={search} placeholder='Search by Currency' onChange={handleChange} />
+                    <div>
+
+                        <input type="text" value={search} placeholder='Search by Currency' onChange={handleChange} />
+                    </div>
+                    <Link to="/view">
+                        <Button className='Button text-light'>SAVED CRYPTO</Button>
+                    </Link>
                 </div>
-                <Link to="/view">
-                <Button  className='Button text-light'>SAVED CRYPTO</Button>
-                </Link>
-            </div>
-            <Table className="Table" aria-label="simple table" stickyHeader>
-                <thead>
-                    <TableRow sx={{ background: "gold", color: "black"}}>
-                    <TableCell align="center" className="tablehead"> Crypto  </TableCell>
-                        <TableCell align="center"> NAME</TableCell>
-                        <TableCell align="center">SYMBOL</TableCell>
-                        <TableCell align="center">MARKET CAP</TableCell>
-                         
+                <Table className="Table" aria-label="simple table" stickyHeader>
+                    <thead>
+                        <TableRow sx={{ background: "gold", color: "black" }}>
+                            <TableCell align="center" className="tablehead"> Crypto  </TableCell>
+                            <TableCell align="center"> NAME</TableCell>
+                            <TableCell align="center">SYMBOL</TableCell>
+                            <TableCell align="center">MARKET CAP</TableCell>
 
-                        <TableCell align="center">CURRENT PRICE</TableCell>
-                        <TableCell align="center">   </TableCell>
 
-                    </TableRow>
-                </thead>
-                <TableBody>
+                            <TableCell align="center">CURRENT PRICE</TableCell>
+                            <TableCell align="center">   </TableCell>
+                            
+                        </TableRow>
+                    </thead>
+                    <TableBody>
+                        {filteredData.slice(0, 15).map((row) => (
+                           
+                            <TableRow className="row-each" key={row.id} onClick={()=>navigate(`/crypto/${row.id}`)} >
+                                <TableCell className="cell text-light" align="center" component="th" scope="row">
+                                    <img className='logo' src={row.image} />
+                                </TableCell>
+                                <TableCell className="cell text-light" align="center" component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="center" className="cell text-light">
+                                    <a className="symbol-btn" >
+                                        {row.symbol}
+                                    </a>
+                                </TableCell>
+                                <TableCell align="center" className="cell text-light">${row.market_cap}</TableCell>
+                                <TableCell align="center" className="cell text-light">${row.current_price.toFixed(2)}
+                                    <p>USD</p>
+                                </TableCell>
 
-                    {filteredData.slice(0, 15).map((row) => (
-            <TableRow className="row-each" key={row.id} onClick={()=>navigate(`/crypto/${row.id}`)} >
-                <TableCell className="cell text-light" align="center" component="th" scope="row">
-                    <img className='logo' src={row.image} />
-                </TableCell>
-                <TableCell className="cell text-light" align="center" component="th" scope="row">
-                    {row.name}
-                </TableCell>
-                <TableCell align="center" className="cell text-light">
-                    <a className="symbol-btn" >
-                        {row.symbol}
-                    </a>
-                </TableCell>
-                <TableCell align="center" className="cell text-light">${row.market_cap}</TableCell>
-                <TableCell align="center" className="cell text-light">${row.current_price.toFixed(2)}
-                    <p>USD</p>
-                </TableCell>
+                                <TableCell>
 
-                <TableCell>
 
-                  
-                <button onClick={() => selected(row.name, row.symbol, row.market_cap, row.current_price,row.image)} 
-                    className="view-btn">SAVE</button>
-                  </TableCell>
+                                    <button onClick={() => selected(row.name, row.symbol, row.market_cap, row.current_price, row.image)}
+                                        className="view-btn">SAVE</button>
+                                </TableCell>
 
-                   
 
-            </TableRow>
+
+                            </TableRow>
+            
         ))}
-    </TableBody>
-   
-</Table>
+                </TableBody>
 
-       
+            </Table>
+
+
         </div>
         </>
     )
